72ebcee8cb75599bec2da0d082e7eaee4171d59e
Aman Singh <chandel.s.aman@gmail.com>
2023-09-30 15:05:10 +0530

    changed team data

--- src/utils/static/teamdata.ts
+++ src/utils/static/teamdata.ts
@@ -1,91 +1,45 @@
 const teamdata = [
   {
-    name: "Sai",
+    name: "Sai Kishan",
     position: "Designer",
     socials: {
-      github: "https://www.github.com/chandel-aman",
-      linkedin: "https://www.linkedin.com/in/chandel-aman/",
+      github: "https://www.github.com/saikishan5000",
+      linkedin: "https://www.linkedin.com/in/saikishan5000/",
     },
     about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fadf",
+      "UI/UX\nI am a learner trying to explore more vast fields and expand my knowledge.",
+    image: "https://avatars.githubusercontent.com/u/93516324?v=4",
   },
   {
-    name: "Aman",
+    name: "Aman Singh",
     position: "Developer",
     socials: {
       github: "https://www.github.com/chandel-aman",
       linkedin: "https://www.linkedin.com/in/chandel-aman/",
     },
-    about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fasdf",
+    about: "MERN stack developer\nTrying hands on AR and 3D.",
+    image: "https://avatars.githubusercontent.com/u/97614113?v=4",
   },
   {
-    name: "Rohit",
-    position: "Lead",
+    name: "Rohit Kumar",
+    position: "Organiser",
     socials: {
-      github: "https://www.github.com/chandel-aman",
-      linkedin: "https://www.linkedin.com/in/chandel-aman/",
+      github: "https://www.github.com/Rohit-554",
+      linkedin: "https://www.linkedin.com/in/rohit115/",
     },
-    about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fadf",
+    about: "Hey there! I'm Rohit, I am a Native Andorid Developer Kotlin.",
+    image: "https://avatars.githubusercontent.com/u/48874687?v=4",
   },
   {
-    name: "Zunaid",
-    position: "Co-lead",
+    name: "Ishita Biswas",
+    position: "GDSC IEM Lead",
     socials: {
-      github: "https://www.github.com/chandel-aman",
-      linkedin: "https://www.linkedin.com/in/chandel-aman/",
+      github: "",
+      linkedin: "https://www.linkedin.com/in/ishistarx/",
     },
     about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fadf",
-  },
-  {
-    name: "Zunaid",
-    position: "Co-lead",
-    socials: {
-      github: "https://www.github.com/chandel-aman",
-      linkedin: "https://www.linkedin.com/in/chandel-aman/",
-    },
-    about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fadf",
-  },
-  {
-    name: "Zunaid",
-    position: "Co-lead",
-    socials: {
-      github: "https://www.github.com/chandel-aman",
-      linkedin: "https://www.linkedin.com/in/chandel-aman/",
-    },
-    about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fadf",
-  },
-  {
-    name: "Zunaid",
-    position: "Co-lead",
-    socials: {
-      github: "https://www.github.com/chandel-aman",
-      linkedin: "https://www.linkedin.com/in/chandel-aman/",
-    },
-    about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fadf",
-  },
-  {
-    name: "Zunaid",
-    position: "Co-lead",
-    socials: {
-      github: "https://www.github.com/chandel-aman",
-      linkedin: "https://www.linkedin.com/in/chandel-aman/",
-    },
-    about:
-      "Lorem ipsum dolor sit ametisal consectetur adipisicing eiusmod tempor incididunt ",
-    image: "fadf",
+      "I'm a management enthusiast exploring businesses with an interest in building the most effective workforce possible.",
+    image: "https://media.licdn.com/dms/image/D4D03AQGJWgToXSe_sw/profile-displayphoto-shrink_400_400/0/1677692271708?e=1701302400&v=beta&t=xYxhwiOu-mV8SG1nVq1xemx_zvksgXVCFbkKxesM5g8",
   },
 ];
 
--- src/utils/ui/TeamCard.tsx
+++ src/utils/ui/TeamCard.tsx
@@ -21,18 +21,21 @@ export function TeamCard(props: ITeamCardProps) {
       className="w-[16rem] h-[16rem] box-border p-6 flex flex-col rounded-[20px] relative bg-[#7a7aba] before:absolute before:top-[-0.25rem] before:left-[-0.25rem] before:box-border before:content-[''] before:w-[16.5rem] before:h-[16.5rem] before:rounded-[24px] before:z-[-1] before:bg-gradient-to-b from-[#9AFFFF] to-[#4D58FF] before:shadow-custom select-none"
     >
       <div className="flex flex-row w-full justify-start items-center gap-8 mb-6">
-        <div className="w-[100px] h-[80px] bg-gradient-to-b from-[#727AFF] to-[#E4C8ED] rounded-[15px] relative">
+        <div
+          className="w-[40px] h-[40px] rounded-[50%] text-black font-bold relative before:absolute before:top-[-2.5px] before:left-[-2.5px] before:box-border before:content-[''] before:w-[45px] before:h-[45px] before:rounded-[50%] before:z-[-1] before:bg-gradient-to-b from-[#9af5ff] to-[#a900cb] cursor-pointer"
+          onClick={() => window.open(props.image, "_blank")}
+        >
           <img
             src={props.image}
-            alt="member-profile"
-            className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[80%]"
+            alt="user-profile-picture"
+            className="w-full h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover rounded-[50%]"
           />
         </div>
         <h3 className="capitalize font-bold text-[1.3rem]">{props.position}</h3>
       </div>
-      <div className="flex flex-col gap-1 w-[90%]">
+      <div className="flex flex-col gap-1 w-[90%] ">
         <h3 className="uppercase font-bold text-[1.2rem]">{props.name}</h3>
-        <p className="text-[0.8rem]">{props.about}</p>
+        <pre className="text-[0.8rem] whitespace-pre-wrap">{props.about}</pre>
       </div>
       <div className="flex flex-col justify-end items-center absolute bottom-8 right-2 gap-4">
         <div
